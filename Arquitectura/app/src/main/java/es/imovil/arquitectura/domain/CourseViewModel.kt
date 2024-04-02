0a160d77be8c12b9d53b5a2a61de26656ad8d0bc
Rattle-Brain <danialgo2001@gmail.com>
2024-04-02 21:14:51 +0200

    9 hecho. CourseApp no existe

--- Arquitectura/app/src/main/java/es/imovil/arquitectura/domain/CourseViewModel.kt
+++ Arquitectura/app/src/main/java/es/imovil/arquitectura/domain/CourseViewModel.kt
@@ -6,23 +6,21 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import es.imovil.arquitectura.data.CourseRepository
 
-class CourseViewModelFactory(private val repository: CourseRepository) :
-    ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(CourseViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return CourseViewModel(repository) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
-}
+
 
 class CourseViewModel(private val repository: CourseRepository): ViewModel() {
     val courseNames: LiveData<List<String>> by lazy {
         repository.getCourseNames().asLiveData()
     }
 
-
-
-
+    class CourseViewModelFactory(private val repository: CourseRepository) :
+        ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(CourseViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return CourseViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
--- Arquitectura/app/src/main/java/es/imovil/arquitectura/ui/ListFragment.kt
+++ Arquitectura/app/src/main/java/es/imovil/arquitectura/ui/ListFragment.kt
@@ -5,21 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import es.imovil.arquitectura.R
+import es.imovil.arquitectura.adapters.CourseListAdapter
 import es.imovil.arquitectura.databinding.FragmentListBinding
+import es.imovil.arquitectura.domain.CourseViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ListFragment : Fragment() {
-
     private var _binding: FragmentListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val courseViewModel: CourseViewModel by viewModels {
+        CourseViewModel.CourseViewModelFactory((activity?.application as CourseApp).repository)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +41,17 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val viewRoot = binding.root
+
+        courseViewModel.courseNames.observe(viewLifecycleOwner) { names ->
+            names.let {
+                val adapter = CourseListAdapter()
+                adapter.submitList(names.toMutableList())
+            }
+        }
+
+        viewRoot.layoutManager = LinearLayoutManager(viewRoot.context)
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_ListFragment_to_CourseFragment)
