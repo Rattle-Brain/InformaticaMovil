016ef6653876742939c7fc6cfad474356c936348
UO278023 <UO278023@uniovi.es>
2024-04-21 19:09:33 +0200

    Added default classes

--- Arquitectura/app/src/main/java/es/imovil/arquitectura/model/CourseDatabase.kt
+++ Arquitectura/app/src/main/java/es/imovil/arquitectura/model/CourseDatabase.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+
 @Database(entities = [Course::class], version = 1)
 abstract class CourseDatabase: RoomDatabase() {
     abstract fun courseDao(): CourseDAO
@@ -37,41 +38,14 @@ abstract class CourseDatabase: RoomDatabase() {
                 super.onCreate(db)
 
                 CoroutineScope(Dispatchers.IO).launch {
-                    INSTANCE?.let { database ->
-                        val courseDao = database.courseDao()
-
-                        // Insertar un curso en la base de datos
-                        courseDao.insertCourse(
-                            Course(
-                                "Ingenieria Movil", "Arias", "Sufrir a muerte"
-                            )
-                        )
-                        courseDao.insertCourse(
-                            Course(
-                                "Sistemas Distribuidos", "JLDiaz", "Sufrimiento divertido"
-                            )
-                        )
-                        courseDao.insertCourse(
-                            Course(
-                                "Isof", "Fanjul", "Aqui estamos para aprender"
-                            )
-                        )
-                        courseDao.insertCourse(
-                            Course(
-                                "Arquitectura", "De la Calle", "5 años sin aprobarla"
-                            )
-                        )
-                        courseDao.insertCourse(
-                            Course(
-                                "PCP", "Ranilla", "Masterclass de cuñao"
-                            )
-                        )
-                        // Realizar otras operaciones con la base de datos si es necesario
-                        // Por ejemplo: insertar más datos, actualizar, eliminar, etc.
-                    }
+                    INSTANCE!!.courseDao().insertCourse(
+                        Course("Android", "Javier", "Curso de Android")
+                    )
+                    INSTANCE!!.courseDao().insertCourse(
+                        Course("iOS", "Javier", "Curso de iOS")
+                    )
                 }
             }
         }
-
     }
 }
