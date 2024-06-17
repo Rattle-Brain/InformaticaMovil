ee3ffb4368504a3eaec59a1220665cfa16869d23
UO278023 <UO278023@uniovi.es>
2024-06-17 20:33:01 +0200

    Add DIFF_CALLBACK obj

--- FiestasAsturias/app/src/main/java/es/imovil/fiestasasturias/adapter/FiestasAdapter.kt
+++ FiestasAsturias/app/src/main/java/es/imovil/fiestasasturias/adapter/FiestasAdapter.kt
@@ -2,11 +2,12 @@ package es.imovil.fiestasasturias.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import es.imovil.fiestasasturias.model.Fiesta
 import es.imovil.fiestasasturias.databinding.FragmentFiestasBinding
 
-class FiestasAdapter () : ListAdapter<Fiesta, FiestasViewHolder>() {
+class FiestasAdapter () : ListAdapter<Fiesta, FiestasViewHolder>(DIFF_CALLBACK) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FiestasViewHolder {
         val itemViewBinding = FragmentFiestasBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return FiestasViewHolder(itemViewBinding)
@@ -14,5 +15,14 @@ class FiestasAdapter () : ListAdapter<Fiesta, FiestasViewHolder>() {
 
     override fun onBindViewHolder(holder: FiestasViewHolder, position: Int) {
         holder.bind(getItem(position))
+    }
+
+    companion object DIFF_CALLBACK: DiffUtil.ItemCallback<Fiesta>() {
+        override fun areItemsTheSame(oldItem: Fiesta, newItem: Fiesta): Boolean {
+            return oldItem.nombre == newItem.nombre
+        }
+        override fun areContentsTheSame(oldItem: Fiesta, newItem: Fiesta): Boolean {
+            return oldItem == newItem
+        }
     }
 }
