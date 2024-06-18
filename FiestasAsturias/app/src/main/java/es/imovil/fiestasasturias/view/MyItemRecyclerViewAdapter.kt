0cad8289fcccacee087c3b09c5b6c200e5d2191c
UO278023 <UO278023@uniovi.es>
2024-06-18 19:41:01 +0200

    Details half done

--- FiestasAsturias/app/src/main/java/es/imovil/fiestasasturias/view/MyItemRecyclerViewAdapter.kt
+++ FiestasAsturias/app/src/main/java/es/imovil/fiestasasturias/view/MyItemRecyclerViewAdapter.kt
@@ -2,7 +2,6 @@ package es.imovil.fiestasasturias.view
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import es.imovil.fiestasasturias.R
