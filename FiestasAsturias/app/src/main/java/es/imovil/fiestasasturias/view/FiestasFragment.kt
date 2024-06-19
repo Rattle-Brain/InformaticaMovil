eb32ad1653508778aef530950dc38b30f939f2a6
UO278023 <danialgo2001@gmail.com>
2024-06-19 14:51:30 +0200

    Small refactor cleanup

--- FiestasAsturias/app/src/main/java/es/imovil/fiestasasturias/view/FiestasFragment.kt
+++ FiestasAsturias/app/src/main/java/es/imovil/fiestasasturias/view/FiestasFragment.kt
@@ -2,10 +2,7 @@ package es.imovil.fiestasasturias.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import es.imovil.fiestasasturias.databinding.FragmentFiestasBinding
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,14 +14,14 @@ class FiestasFragment : Fragment() {
 
     private var _binding: FragmentFiestasBinding? = null
     private val binding get() = _binding!!
-    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var mainFargment: NavHostFragment
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // init de las referencias al navHostFragment
-        navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.mainNavFragment) as NavHostFragment
-        navController = navHostFragment.navController
+        mainFargment = requireActivity().supportFragmentManager.findFragmentById(R.id.mainNavFragment) as NavHostFragment
+        navController = mainFargment.navController
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
